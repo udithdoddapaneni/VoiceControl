6fb5e0cca3f84c30450c04b299224e1855923606
Dhruvadeep Malakar <dhruvadeepmalakar12345@gmail.com>
2025-01-22 11:33:23 +0530

    works for windows

--- HardwareControl/screenshot.py
+++ HardwareControl/screenshot.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python3
+import pyautogui
+from datetime import datetime
 
-import datetime
-import pyscreenshot as ImageGrab  # pip install pyscreenshot
+# Generate a timestamp for the filename
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = f"{timestamp}.png"
 
-def capture_screenshot():
-    # Get timestamp in YYYYmmdd_HHMMSS format
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    
-    # Construct file name
-    filename = f"screenshot_{timestamp}.png"
-    
-    # Capture the entire screen
-    image = ImageGrab.grab()
-    
-    # Save in the same directory as this Python file
-    image.save(filename)
-    print(f"Screenshot saved as {filename}")
+# Take a screenshot
+screenshot = pyautogui.screenshot()
 
-if __name__ == "__main__":
-    capture_screenshot()
+# Save the screenshot
+screenshot.save(filename)
+
+print(f"Screenshot saved as {filename}")
